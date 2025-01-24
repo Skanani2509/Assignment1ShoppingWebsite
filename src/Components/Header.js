a4fc8f41785a246cd4465ea2241c616dac88f84a
Skanani2509 <smitkanani2509@gmail.com>
2025-01-24 18:29:58 -0500

    updated

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = ({ user, logout}) => {
+    return (
+        <header>
+            <nav>
+            <Link to="/" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Home</Link>
+            <Link to="/cart" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Cart</Link>
+            <Link to="/account" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Account</Link>
+            <Link to="/comments" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Comments</Link>
+            {user ? (
+                <span>
+                    Welcome, {user.name} <button onClick={logout} style={{ marginLeft: "15px", cursor: "pointer"}}>Logout</button>
+                </span>
+            ) : (
+                <Link to="/login" style={{ color: "#fff", textDecoration: "none" }}>Login</Link>
+            )}
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
