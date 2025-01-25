b3541c0ce600c4a52cdc3c809bda22d7810dc3b2
Skanani2509 <smitkanani2509@gmail.com>
2025-01-24 21:29:40 -0500

    add user details

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 const Account = ({ user, saveUser }) => {
   const [name, setName] = useState(user?.name || "");
+  const [email, setEmail] = useState(user?.email || "");
+  const [shippingAddress, setShippingAddress] = useState(user?.shippingAddress || "");
+  const [password, setPassword] = useState("");
 };
 
 export default Account;
