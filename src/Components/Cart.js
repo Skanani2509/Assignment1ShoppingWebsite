f7a90945d1e42e3f498e55b843a50b7ebd737939
Astha156 <astharajan1506@gmail.com>
2025-01-24 21:42:00 -0500

    handle quantity

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Cart = ({ cart, updateCart, finalizePurchase }) => {
     const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    const handleQuantityChange = (id, qty) => {
+        updateCart(id, qty);
+      };
   
   };
   
