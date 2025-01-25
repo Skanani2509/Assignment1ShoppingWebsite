0a06593ad7df6b5194fc342937b5e2a9973f0dc6
Skanani2509 <smitkanani2509@gmail.com>
2025-01-24 22:02:14 -0500

    updated

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -16,6 +16,32 @@ const Account = ({ user, saveUser }) => {
     alert(user ? "Account updated successfully!" : "Account created successfully!");
   };
 
+  return (
+    <div>
+      <h1>{user ? "Edit Account" : "Create Account"}</h1>
+      <div>
+        <label>Name:</label>
+        <input type="text" placeholder="Name"/>
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" placeholder="Email"/>
+      </div>
+      <div>
+        <label>Shipping Address:</label>
+        <input type="text" placeholder="Shipping Address"/>
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" placeholder="Password"/>
+      </div>
+      <button
+        onClick={handleSave}>
+        {user ? "Save Changes" : "Create Account"}
+      </button>
+    </div>
+  );
+
 };
 
 
