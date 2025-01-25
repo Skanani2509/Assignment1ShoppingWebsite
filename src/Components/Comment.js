2e730b59d7cfe081d660e322d411acbe47057f41
Astha156 <astharajan1506@gmail.com>
2025-01-24 22:51:29 -0500

    add comment

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
 
 
+const Comments = ({ comments, addComment }) => {
+    const [newComment, setNewComment] = useState("");
+    const [image, setImage] = useState(null);
+
+}; 
+
+
 export default Comments;
