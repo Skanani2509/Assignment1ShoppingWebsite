c1db1dc993981059943843a41359ff61a917bcb7
Astha156 <astharajan1506@gmail.com>
2025-01-24 22:58:10 -0500

    handlecomments

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -5,7 +5,22 @@ const Comments = ({ comments, addComment }) => {
     const [newComment, setNewComment] = useState("");
     const [image, setImage] = useState(null);
 
-}; 
+    const handleCommentSubmit = () => {
+        if (newComment.trim() || image) {
+          addComment({ text: newComment, image });
+          setNewComment("");
+          setImage(null);
+        }
+      };
+    
+      return (
+        <div>
+          <h1>Comments</h1>
+        </div>
+            )
+          
+    };
+    
 
 
 export default Comments;
