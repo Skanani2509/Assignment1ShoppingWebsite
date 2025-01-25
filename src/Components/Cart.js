bce623beafd1ff3b882ee746bd3ee708d0df7bbd
Astha156 <astharajan1506@gmail.com>
2025-01-24 21:40:03 -0500

    cart created

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+const Cart = ({ cart, updateCart, finalizePurchase }) => {
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  
+  };
+  
+  export default Cart;
+  
