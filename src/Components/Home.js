8a832b81132e139e16de5471ae1d2fa1e7b9365f
nirhem <nirhem3@gmail.com>
2025-01-25 21:48:01 -0500

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,5 +2,6 @@ import React, {useState} from "react";
 import {products} from "../data/products";
 
 const Home = ({ addToCart }) => {
+    const [quantities, setQuantities] = useState({});
     
 };
