e1d294c7f3040cb6803ec12a7e39f51e35b92ec9
nirhem <nirhem3@gmail.com>
2025-01-25 22:23:42 -0500

    updated

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,4 +1,7 @@
 export const products = [
     { id: 1, name: "Apple", description: "Fresh apples", price: 20, image: "img1.jpeg" },
-    
-]
+    { id: 1, name: "Apple", description: "Fresh apples", price: 20, image: "img1.jpeg" },
+    { id: 1, name: "Apple", description: "Fresh apples", price: 20, image: "img1.jpeg" },
+    { id: 1, name: "Apple", description: "Fresh apples", price: 20, image: "img1.jpeg" },
+    { id: 1, name: "Apple", description: "Fresh apples", price: 20, image: "img1.jpeg" },
+];
