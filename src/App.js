7d5a360449f15d5c3e79868769b521538d0b33d1
nirhem <nirhem3@gmail.com>
2025-01-26 05:14:16 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home";
+import Cart from "./pages/Cart";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
-  );
+  const [cart, setCart] = useState([]);
+  const [user, setUser] = useState(null);
+  const [comments, setComments] = useState([]);
 }
 
 export default App;
