7ccc5e0091f7cf25e5954d0c242312f1d5a74a98
nirhem <nirhem3@gmail.com>
2025-01-26 20:35:48 -0500

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const Header = ({ user, logout}) => {
     return (
-        <header>
+        <header style={{ padding: "10px 20px", backgroundColor: "#cf5c36", color: "#d3d5d7" }}>
             <nav>
-            <Link to="/" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Home</Link>
-            <Link to="/cart" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Cart</Link>
-            <Link to="/account" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Account</Link>
-            <Link to="/comments" style={{ marginRight: "15px", color: "#fff", textDecoration: "none" }}>Comments</Link>
-            {user ? (
-                <span>
-                    Welcome, {user.name} <button onClick={logout} style={{ marginLeft: "15px", cursor: "pointer"}}>Logout</button>
-                </span>
-            ) : (
-                <Link to="/login" style={{ color: "#fff", textDecoration: "none" }}>Login</Link>
-            )}
+                <Link to="/" style={{ marginRight: "15px", color: "#d3d5d7", textDecoration: "none" }}>Home</Link>
+                <Link to="/cart" style={{ marginRight: "15px", color: "#d3d5d7", textDecoration: "none" }}>Cart</Link>
+                <Link to="/account" style={{ marginRight: "15px", color: "#d3d5d7", textDecoration: "none" }}>Account</Link>
+                <Link to="/comments" style={{ marginRight: "15px", color: "#d3d5d7", textDecoration: "none" }}>Comments</Link>
+                {user ? (
+                    <span>
+                        Welcome, {user.name} <button onClick={logout} style={{ marginLeft: "15px", cursor: "pointer"}}>Logout</button>
+                    </span>
+                ) : (
+                    <Link to="/login" style={{ color: "#d3d5d7", textDecoration: "none" }}>Login</Link>
+                )}
             </nav>
         </header>
     );
